33a8c09b796b5c929926471fd32f2152b46d0e92
maruthi7570 <maruthiashok52@gmail.com>
2025-04-02 15:27:23 +0530

    intelj files

---
+++ bank/src/com/xworkz/bank/Branch/Branch.java
@@ -0,0 +1,23 @@
+package com.xworkz.bank.Branch;
+
+
+import com.xworkz.bank.Bank.BankAccount;
+import com.xworkz.bank.Bank.BankAccount;
+
+public class Branch {
+        public int branchId;
+        public String branchName;
+        public String location;
+        public BankAccount[] bankAccounts;
+
+        public void displayBranchDetails() {
+            System.out.println("Branch ID: " + branchId);
+            System.out.println("Branch Name: " + branchName);
+            System.out.println("Location: " + location);
+            System.out.println("Accounts in this Branch:");
+            for (BankAccount account : bankAccounts) {
+                account.displayAccountDetails();
+            }
+        }
+    }
+
